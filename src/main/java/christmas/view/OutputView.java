ce50d1874f76e9a1957d06f0574b1ee23639f517
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-11-13 16:15:15 +0900

    feat(output): 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -7,27 +7,34 @@ import java.util.Map;
 public class OutputView {
     private static final String NEWLINE = System.lineSeparator();
 
-    //기본 메시지 출력 메소드
     public void printMessage(ConstantsMessage message) {
+        System.out.println(message.getMessage());
     }
 
-    //주문 메뉴 출력
     public void printMenu(List<Map<String, Integer>> orderMenus) {
-
+        StringBuilder menuBuilder = new StringBuilder();
+        menuBuilder.append(ConstantsMessage.ORDER_MENU);
+        for (Map<String, Integer> menu : orderMenus) {
+            menuBuilder.append(menu).append(NEWLINE);
+        }
+        menuBuilder.append(NEWLINE);
+        System.out.println(menuBuilder);
     }
 
-    //금액 관련 출력
     public void printAmount(ConstantsMessage message, int amount) {
-
+        System.out.println(message.getMessage());
+        System.out.println(amount + "원");
     }
 
     //증정 메뉴 출력
-    public void printGift() {
-
+    public void printGift(String giftProduct) {
+        System.out.println(ConstantsMessage.GIFT_MENU);
+        System.out.println(giftProduct + "1개");
     }
 
     //배지 출력
     public void printBadge(String badge) {
-
+        System.out.println(ConstantsMessage.EVENT_BADGE);
+        System.out.println(badge);
     }
 }
