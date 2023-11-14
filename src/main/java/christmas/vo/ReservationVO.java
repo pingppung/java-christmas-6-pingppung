26b7763d7c5b29821ce29bb937cc42efed290acd
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-11-14 16:46:39 +0900

    feat(reserve): 식당 예약 정보를 저장할 수 있는 구조 생성

---
+++ src/main/java/christmas/vo/ReservationVO.java
@@ -0,0 +1,29 @@
+package christmas.vo;
+
+import christmas.enums.EventDate;
+import java.time.LocalDate;
+import java.util.List;
+
+public class ReservationVO {
+    private final LocalDate reservationDate;
+    private final List<OrderMenuVO> orderMenu;
+    private final List<EligibleEventVO> eligibleEvents;
+
+    private ReservationVO(LocalDate reservationDate, List<OrderMenuVO> orderMenu,
+                          List<EligibleEventVO> eligibleEvents) {
+        this.reservationDate = reservationDate;
+        this.orderMenu = orderMenu;
+        this.eligibleEvents = eligibleEvents;
+    }
+
+    public static ReservationVO create(int reservationDay, List<OrderMenuVO> orderMenu,
+                                       List<EligibleEventVO> eligibleEvents) {
+        LocalDate reservationDate = LocalDate.of(EventDate.CHRISTMAS_DAY.getYear(),
+                EventDate.CHRISTMAS_DAY.getMonthValue(), reservationDay);
+        return new ReservationVO(reservationDate, orderMenu, eligibleEvents);
+    }
+
+
+}
+
+
